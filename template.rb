bf4a9fd3a4df5613cc7b982f419524181e336df1
Meng-Yang Lee <crazylion2@gmail.com>
2015-10-31 15:11:40 +0800

    加入i18n 跟一些常用的gem

--- template.rb
+++ template.rb
@@ -10,18 +10,25 @@ run "touch Gemfile"
 #be sure to add source at the top of the file
 add_source 'https://rubygems.org'
 gem 'rails', '4.2.4'
+gem 'rails-i18n', '~> 4.0.0'
 gem 'uglifier'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'devise'
+gem 'devise-i18n'
 gem 'cancancan'
 gem 'bootstrap-sass'
 gem 'sass-rails'
 gem 'compass-rails'
 gem 'simple_form'
 gem 'kaminari'
+gem 'kaminari-i18n'
 gem 'sdoc', group: :doc
 gem 'bulk_insert'  # 大量匯入用
+
+#搜尋
+gem 'ransack'
+
 #gem and gem_group will work from Rails Template API
 gem_group :development, :test do
     gem 'sqlite3'
@@ -33,6 +40,10 @@ gem_group :development, :test do
     gem 'better_errors'
     gem 'binding_of_caller'
     gem 'faker'
+end
+
+gem_group  :production do
+    gem 'exception_notification'
 end
 
 run "bundle"
