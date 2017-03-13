5aecefdb32dab3ba01a8b5fe75843ab55189f92b
張旭 <zx1986@gmail.com>
2017-03-13 20:07:32 +0800

    整理一下排版，區分群組，移除幾個 gem，增加 aasm, pundit 等 gem
    http://guides.rubyonrails.org/rails_application_templates.html

--- template.rb
+++ template.rb
@@ -1,49 +1,64 @@
 def source_paths
-      Array(super)
-        [File.expand_path(File.dirname(__FILE__))]
+  [File.expand_path(File.dirname(__FILE__))]
 end
-
-
 
 remove_file "Gemfile"
 run "touch Gemfile"
-#be sure to add source at the top of the file
+
+# be sure to add source at the top of the file
 add_source 'https://rubygems.org'
-gem 'rails', '4.2.4'
-gem 'rails-i18n', '~> 4.0.0'
-gem 'uglifier'
+
+gem 'rails', '4.2.8'
+gem 'rails-i18n'
+
 gem 'jquery-rails'
-gem 'turbolinks'
-gem 'devise'
-gem 'devise-i18n'
-gem 'cancancan'
-gem 'bootstrap-sass'
 gem 'sass-rails'
 gem 'compass-rails'
-gem 'simple_form'
+
+gem 'mysql2'
+gem 'uglifier'
+# gem 'jbuilder'
+# gem 'turbolinks'
+
+gem 'devise'
+gem 'devise-i18n'
+
 gem 'kaminari'
 gem 'kaminari-i18n'
-gem 'sdoc', group: :doc
-gem 'bulk_insert'  # 大量匯入用
 
-#搜尋
+gem 'aasm'
+gem 'pundit'
 gem 'ransack'
+gem 'paperclip'
+gem 'paper_trail'
+gem 'simple_form'
 
-#gem and gem_group will work from Rails Template API
-gem_group :development, :test do
-    gem 'sqlite3'
-    gem 'spring'
-    gem 'quiet_assets'
-    gem 'pry-rails'
-    gem 'byebug'
-    gem 'awesome_print'
-    gem 'better_errors'
-    gem 'binding_of_caller'
-    gem 'faker'
+gem 'bulk_insert'
+gem 'hipchat'
+
+gem_group :test do
+  gem 'sqlite3'
 end
 
-gem_group  :production do
-    gem 'exception_notification'
+gem_group :development, :test do
+  gem 'web-console'
+  gem 'quiet_assets'
+  gem 'better_errors'
+  gem 'awesome_print'
+  gem 'byebug'
+  gem 'faker'
+  gem 'sdoc'
+  gem 'pry'
+  gem 'pry-rails'
+  gem 'binding_of_caller'
+  gem 'guard'
+  gem 'guard-livereload'
+  gem 'capistrano'
+  gem 'rails-erd'
+end
+
+gem_group :production do
+  gem 'exception_notification'
 end
 
 run "bundle"
@@ -54,6 +69,9 @@ generate 'devise:views'
 generate 'simple_form:install --bootstrap'
 
 rake 'db:migrate'
+
 git :init
-git add: '.'
+git add: "."
+git commit: "-a -m 'Initial commit by Rails bootstrap script.'"
+
 git flow init
